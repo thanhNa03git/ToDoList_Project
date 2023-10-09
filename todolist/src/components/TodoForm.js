81115088e2ac5f17b801d790021b0ddbac7957bc
thanhNa <vothanhna.work@gmail.com>
2023-10-09 15:59:46 +0700

    update todoForm

--- todolist/src/components/TodoForm.js
+++ todolist/src/components/TodoForm.js
@@ -1,17 +1,28 @@
 import React, {useState} from 'react';
-export const TodoForm = () => {
-    const [value, setValue] = useState("")
-    const handleSubmit = e => {
-        e.preventDefault();
 
-        console.log(value);
-    }
+export const TodoForm = ({addTodo}) => {
+    const [value, setValue] = useState("");
+
+    const handleSubmit = e => {
+        // block hang dong default
+        e.preventDefault();
+        // check console
+        console.log("show value: ",value);
+        if(value) {
+            // Them task
+            addTodo(value);
+            // clean form input
+            setValue('');
+        }
+    };
     return(
-        <from className= 'TodoForm' onSubmit = {handleSubmit} >
+        <form className= 'TodoForm' onSubmit = {handleSubmit} >
             <input type='text' className='todo-input' 
             placeholder='What is the task today ?'
-            onChange = {(e) => setValue(e.target.value)}/>
+            value= {value}
+            onChange={(e) => setValue(e.target.value)}/>
+            
             <button type='submit' className='todo-btn'>Add Task</button>
-        </from>
+        </form>
     )
 }
