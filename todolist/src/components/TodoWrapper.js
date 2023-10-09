43320b036ec910d3d58ad00dc04625a93ddcf037
thanhNa <vothanhna.work@gmail.com>
2023-10-09 16:20:37 +0700

    Add todo

--- todolist/src/components/TodoWrapper.js
+++ todolist/src/components/TodoWrapper.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
+import { Todo } from "./Todo";
 uuidv4();
 
 export const TodoWrapper = () => {
@@ -16,7 +17,12 @@ export const TodoWrapper = () => {
 
     return (
         <div className="TodoWrapper">
-        <TodoForm addTodo={addTodo} />
+            <TodoForm addTodo={addTodo} />
+            
+            {todos.map((todo,index) => (
+                <Todo task={todo} key={index}/>
+            ))}
+        
         </div>
     );
 };
